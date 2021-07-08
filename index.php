d30210d94d6d0e900d44a4a3793f749fb00b1c19
Pj Malva <pjmalva@gmail.com>
2021-07-08 15:50:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ Author URI: https://github.com/pjmalva
 Version: v0.0.1
 */
 
-require_once( __DIR__ . '\view.php' );
-require_once( __DIR__ . '\logging.php' );
-require_once( __DIR__ . '\users.php' );
-require_once( __DIR__ . '\database.php' );
-require_once( __DIR__ . '\configurations.php' );
+require_once( __DIR__ . '/view.php' );
+require_once( __DIR__ . '/logging.php' );
+require_once( __DIR__ . '/users.php' );
+require_once( __DIR__ . '/database.php' );
+require_once( __DIR__ . '/configurations.php' );
 
 register_activation_hook( __FILE__ , 'db_install' );
